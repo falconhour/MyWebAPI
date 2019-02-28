d98adb3984ed67ba29c454deed5ee9eb38f32ae5
Ken <43943506+falconhour@users.noreply.github.com>
2019-02-28 15:11:57 +0800

    test

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace MyWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var s = "tset";
             services.AddCors(options =>
             {
                 options.AddPolicy(MyAllowSpecificOrigins,
